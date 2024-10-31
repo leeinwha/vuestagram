45cf062c2748f472b9cb2863a084ba0b66388d92
iNa. <aljard@naver.com>
2024-10-31 22:00:42 +0900

    VUE SNS project (ajax)

---
+++ src/components/ContainerSection.vue
@@ -0,0 +1,22 @@
+<template>
+  <div>
+    <PostSection :게시물="게시물[i]" v-for="(a,i) in 게시물" :key="i"/>   
+  </div>
+</template>
+
+<script>
+import PostSection from './PostSection.vue';
+
+export default {
+  components : {
+    PostSection
+  },
+  props : {
+    게시물 : Array,
+  }
+}
+</script>
+
+<style>
+
+</style>
